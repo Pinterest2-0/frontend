c7e666e2a147662ed22b159f2d1e95e009576304
Omarius Credle <omariuscredle@omariuss-air.home>
2021-06-18 19:44:55 -0400

    Added global dark mode

--- frontend2-3/src/App.js
+++ frontend2-3/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./Components/GlobalStyles";
+import { lightTheme, darkTheme } from "./Components/Themes"
+import  {useDarkMode} from "./Hooks/useDarkMode";
+import Toggle from './Components/Toggler'; 
 import {Route, Link, Switch } from 'react-router-dom'; 
 import './App.css';
 import Dashboard from './Components/Dashboard'; 
+import {useState} from 'react'; 
 
 function App() {
+  
+
+  const [theme, themeToggler] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
+    <ThemeProvider theme={themeMode}>
+      <>
+      <GlobalStyles/>
     <div className="App">
+    <Toggle theme={theme} toggleTheme={themeToggler} />
       <h1>Parent Component</h1>
       <h3>Welcome to article efficiency</h3>
       <Link to='/userdashboard'>Access Test Userdashboard</Link>
@@ -15,6 +30,9 @@ function App() {
         </Route>
       </Switch>
     </div>
+
+</>
+</ThemeProvider>
   );
 }
 
