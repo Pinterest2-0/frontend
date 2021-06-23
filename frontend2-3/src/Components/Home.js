a5fea37c3751762b98ecec308b5737f5a061a5c4
Omarius <70237021+credleo95@users.noreply.github.com>
2021-06-23 08:17:34 -0400

    Add files via upload

---
+++ frontend2-3/src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CategoryList from './CategoryList';
+import Feed from './Feed';
+
+export default function Home() {
+    return (
+        <div>
+            <h2>Sort by Category</h2>
+            <CategoryList />
+            <hr />
+        
+            <div className='feed-container'>
+                <Feed />
+            </div>
+        </div>
+    )
+}
